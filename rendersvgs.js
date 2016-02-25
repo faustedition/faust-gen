b10cdd3b9350cd143225717ee94203833a300619
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2016-02-25 15:17:49 +0100

    Made rendersvgs parameterizeable

--- rendersvgs.js
+++ rendersvgs.js
@@ -4,25 +4,38 @@
  */
 var page = require('webpage').create(),
     fs = require('fs'),
-    system = require('system'),
+    system = require('system');
 
-    transcriptFile = "/home/tv/workspace/faust-gen-all/target/transcript/gsa/391098/0101.xml.json";
+var backend = system.args[1],
+    input   = system.args[2],
+    output  = system.args[3],
+    links   = system.args[4];
+
+if (system.args.length < 4 || system.args.length > 5) {
+  console.log('Usage: rendersvgs.js backend input output [links]');
+  console.log('  backend: http URL to the transcript-generation.html');
+  console.log('  input:   JSON file of the page transcript');
+  console.log('  output:  output svg file, will be written');
+  console.log('  links:   text image link svg, if given, will write overlay svg');
+  phantom.exit(1);
+}
+
 
 page.onError = function (msg, trace) { console.log(msg); trace.forEach(function(item) { console.log(' ', item.file, ':', item.line); }); }; 
 page.onConsoleMessage = function(msg, lineNum, sourceId) { console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")'); };
 page.onCallback = function(svgString) {
-      out = fs.open("test-callback.svg", { mode: "w", charset: "UTF-8" });
+      out = fs.open(output, { mode: "w", charset: "UTF-8" });
       out.write(svgString);
       out.close();
       phantom.exit();
 };
 
-page.open("http://localhost/faust-gen/transcript-generation.html", function(status) {
-  var transcript = fs.read(transcriptFile);
+page.open(backend, function(status) {
+  var transcript = fs.read(input);
 
-  function doRender(transcript) {
+  function renderTranscript(transcript) {
     transcriptGeneration.createDiplomaticSvg(transcript, function(svg) {});
   };
 
-  page.evaluate(doRender, transcript);
+  page.evaluate(renderTranscript, transcript);
 });
