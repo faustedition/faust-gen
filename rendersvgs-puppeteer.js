1e56d91a7dbf0b429fe55a7ff7cc4cdac6a6ef38
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2020-03-31 16:25:39 +0200

    timeout for the rendering function

--- rendersvgs-puppeteer.js
+++ rendersvgs-puppeteer.js
@@ -13,39 +13,51 @@ if (process.argv.length !== 5 && process.argv.length !== 7) {
   console.log('Usage: node rendersvgs.js backend input output [links linkoutput]');
 } else {
 
-  (async () => {
-    try {
-    const browser = await puppeteer.launch({args: ['--no-sandbox'], /* headless: false, slowMo: 100, devtools: true, */ });
-    const page = await browser.newPage();
+    (async () => {
+        try {
+            const browser = await puppeteer.launch({args: ['--no-sandbox'], /* headless: false, slowMo: 100, devtools: true, */ });
+            try {
+                const page = await browser.newPage();
 
-    page.on('console', msg => {
-      for (let i = 0; i < msg.args().length; ++i)
-        console.log(`${i}: ${msg.args()[i]}`);
-    });
+                page.on('console', msg => {
+                    for (let i = 0; i < msg.args().length; ++i)
+                        console.log(`${i}: ${msg.args()[i]}`);
+                });
 
-    await page.goto(backend, {waitUntil: "domcontentloaded"});
+                await page.goto(backend, {waitUntil: "domcontentloaded"});
 
-    const transcript = await fsP.readFile(input, { encoding: "utf-8"}),
-          imagelinks = links? fsP.readFile(links, {encoding: "utf-8"}) : undefined;
+                const transcript = await fsP.readFile(input, {encoding: "utf-8"}),
+                    imagelinks = links ? fsP.readFile(links, {encoding: "utf-8"}) : undefined;
 
-    await page.waitForFunction("function() {return typeof(transcriptGeneration.createPromise) === 'function'}")
-    await page.$eval("#preload", el => el.parentNode.removeChild(el));
-    const result = await page.evaluate((t, i) => { return transcriptGeneration.createPromise(t, i); }, transcript, imagelinks);
+                await page.waitForFunction("function() {return typeof(transcriptGeneration.createPromise) === 'function'}")
+                await page.$eval("#preload", el => el.parentNode.removeChild(el));
+                const result = await page.evaluate((t, i) => {
+                        return Promise.race([
+                            transcriptGeneration.createPromise(t, i),
+                            new Promise((_, reject) => setTimeout(reject, 30000, 'Rendering took more than 30 seconds'))
+                            ]);
+                    }, transcript, imagelinks);
 
-    await fsP.writeFile(output, result.svg, {encoding: "utf-8"});
-    if (linkout) {
-      await fsP.writeFile(linkout, result.overlay, {encoding: "utf-8"});
-    }
+                await fsP.writeFile(output, result.svg, {encoding: "utf-8"});
+                if (linkout) {
+                    await fsP.writeFile(linkout, result.overlay, {encoding: "utf-8"});
+                }
 
-    await page.pdf({path: output.replace(/.svg$/, ".pdf"),
-                    format: 'A4', 
-                    margin: '6pt'}); 
-    await page.screenshot({path: output.replace(/.svg$/, ".png"),
-                    fullPage: true}); 
-    browser.close();
-    } catch (e) {
-      console.error(e);
-    }
+                await page.pdf({
+                    path: output.replace(/.svg$/, ".pdf"),
+                    format: 'A4',
+                    margin: ['2cm', '2cm', '2cm', '2cm']
+                });
+                await page.screenshot({
+                    path: output.replace(/.svg$/, ".png"),
+                    fullPage: true
+                });
+            } finally {
+                browser.close();
+            }
+        } catch (e) {
+            console.error(e);
+        }
 
-  })();
+    })();
 }
